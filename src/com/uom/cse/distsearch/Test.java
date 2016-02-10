4e6f0020c41dd3e0b471ad39bc6844d27e4422e4
Gobinath <slgobinath@gmail.com>
2016-02-10 16:52:20 +0530

    Java version is changed to 1.7

--- src/com/uom/cse/distsearch/Test.java
+++ src/com/uom/cse/distsearch/Test.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import com.uom.cse.distsearch.model.NodeInfo;
 import com.uom.cse.distsearch.util.Utility;
@@ -26,47 +25,44 @@ public class Test {
 
 			Utility.printToFile("");
 
-			String movies = node.getMovies().stream().collect(Collectors.joining(", "));
+			String movies = node.getMovies().toString();
 
 			Utility.printToFile("Movies: " + movies);
 
 			Utility.printToFile("");
 
 			System.out.println("Please hit the enter to continue testing...");
+			try (Scanner sc = new Scanner(System.in)) {
+				while (sc.hasNextLine()) {
 
-			try (Scanner scanner = new Scanner(System.in)) {
-				if (scanner.hasNextLine()) {
-					scanner.nextLine();
+					sc.nextLine();
+
+					List<NodeInfo> peersList = node.getPeerList();
+					int count = peersList.size();
+					Utility.printToFile("Number of peers: " + count);
+					if (count > 0) {
+						String peers = peersList.toString();
+						Utility.printToFile("Peers: " + peers);
+					}
+
+					Utility.printToFile("\r\n\r\n\r\n");
+
+					System.out.println("Start searching...");
+
+					try (Scanner scanner = new Scanner(new File(QUERY_FILE))) {
+						while (scanner.hasNextLine()) {
+							String query = scanner.nextLine().trim().replace(" ", "%20");
+							node.search(query);
+							sleep();
+						}
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("Stop searching");
 				}
 			}
-
-			List<NodeInfo> peersList = node.getPeerList();
-			int count = peersList.size();
-			Utility.printToFile("Number of peers: " + count);
-			if (count > 0) {
-				String peers = peersList.stream().map(x -> x.getIp() + ":" + x.getPort())
-						.collect(Collectors.joining(", "));
-				Utility.printToFile("Peers: " + peers);
-			}
-
-			Utility.printToFile("\r\n\r\n\r\n");
-
-			System.out.println("Start searching...");
-
-			try (Scanner scanner = new Scanner(new File(QUERY_FILE))) {
-				while (scanner.hasNextLine()) {
-					String query = scanner.nextLine().trim().replace(" ", "%20");
-					node.search(query);
-					sleep();
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Stop searching");
 		}
-
-		System.out.println("Testing is finished successfully.");
 	}
 
 	public static void sleep() {
